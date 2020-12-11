7e78fe7767d382fbb593fa8e1d5d11f2fc3c1d7f
hyungpil <kimhyungpil82@gmail.com>
2020-12-11 13:09:54 +0900

    proxy 주석

--- front/src/setupProxy.js
+++ front/src/setupProxy.js
@@ -1,15 +1,15 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function (app) {
-  app.use(
-    proxy('/api', {
-      target: 'http://lottoserver:3002'
-    })
-  )
+  // app.use(
+  //   proxy('/api', {
+  //     target: 'http://lottoserver:3002'
+  //   })
+  // )
 
-  app.use(
-    proxy('/auth', {
-      target: 'http://lottoserver:3002'
-    })
-  )
+  // app.use(
+  //   proxy('/auth', {
+  //     target: 'http://lottoserver:3002'
+  //   })
+  // )
 }
