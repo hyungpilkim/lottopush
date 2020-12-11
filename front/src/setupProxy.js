d0e30923fc192d640fac34314c2baa0e0c1ac354
hyungpil <kimhyungpil82@gmail.com>
2020-12-11 13:31:33 +0900

    url 수정

--- front/src/setupProxy.js
+++ front/src/setupProxy.js
@@ -1,15 +1,15 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function (app) {
-  // app.use(
-  //   proxy('/api', {
-  //     target: 'http://lottoserver:3002'
-  //   })
-  // )
+  app.use(
+    proxy('/api', {
+      target: 'http://115.68.226.104/:3002'
+    })
+  )
 
-  // app.use(
-  //   proxy('/auth', {
-  //     target: 'http://lottoserver:3002'
-  //   })
-  // )
+  app.use(
+    proxy('/auth', {
+      target: 'http://115.68.226.104:3002'
+    })
+  )
 }
